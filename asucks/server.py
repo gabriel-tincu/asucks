8522bb062919877fff3962184194564d429c48c9
Gabriel Tincu <gabi@aiven.io>
2020-11-20 17:33:04 +0100

    cli: tweak parameter parsing
    - socket use is now parsed as a flag
    - port is the proper int type

--- asucks/server.py
+++ asucks/server.py
@@ -12,15 +12,16 @@ log = logging.getLogger(__name__)
 
 def main():
     parser = argparse.ArgumentParser(description="SOCKS5 server", prog="asucks.server")
-    parser.add_argument("--port", default=1080, help="Server port")
+    parser.add_argument("--port", default=1080, type=int, help="Server port")
     parser.add_argument("--host", default="0.0.0.0", help="Network interface")
     parser.add_argument("--username", default=None, help="Username for user/pass auth")
     parser.add_argument("--password", default=None, help="Password for user/pass auth")
     parser.add_argument("--validator", default=None, help="External validator url")
     parser.add_argument("--cafile", default=None, help="Remote validator certificate file")
     parser.add_argument("--log-level", default="INFO", help="Log level visible")
-    parser.add_argument("--use-sockets", default=False, help="Use the base socket server implementation")
+    parser.add_argument("--use-sockets", default=False, action="store_true", help="Use the base socket server implementation")
     args = parser.parse_args(None)
+    logging.basicConfig(level=args.log_level)
 
     config = ServerConfig(
         username=args.username,
@@ -30,11 +31,10 @@ def main():
         validator=args.validator,
         ca_file=args.cafile,
     )
-    asyncio.run(run_main(config=config, use_sockets=args.use_sockets, log_level=args.log_level))
+    asyncio.run(run_main(config=config, use_sockets=args.use_sockets))
 
 
-async def run_main(config: ServerConfig, use_sockets: bool, log_level: str):
-    logging.basicConfig(level=log_level)
+async def run_main(config: ServerConfig, use_sockets: bool):
     loop = asyncio.get_running_loop()
     closing = asyncio.Event()
     if use_sockets:
